d6b68b6ebf93950f352fbadf14fdbe16834fc32a
hIt Vir <hienhao021198@gmail.com>
2023-03-14 02:20:57 +0700

    update: init project

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', 'jsx', '.ios.js', '.android.js'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console', 'ignite-ignore-reactotron'],
+    },
+  },
+};
